514b79a9dbe5202e3333d2b5a999899aab24125f
ckaznable <kami25565@gmail.com>
2023-03-14 10:28:52 +0800

    feat: add help widget

---
+++ src/widget/help.rs
@@ -0,0 +1,35 @@
+use tui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    style::{Color, Style},
+    widgets::{Block, Borders, Clear, Paragraph, Widget},
+};
+
+pub struct Help;
+
+impl Widget for Help {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        Clear.render(area, buf);
+
+        let block = Block::default()
+            .style(Style::default().bg(Color::Gray))
+            .title_alignment(Alignment::Center)
+            .title("Help")
+            .borders(Borders::ALL);
+
+        let text = "
+Use arrow keys or hjkl to move\n
+Press page up or page down to move 4 lines at a time\n
+Press q to exit\n
+Press h for help\n
+Press gg to go to the top and G to go to the bottom\n
+Use / to enter search mode\n\n\n\n
+Press h to exit help";
+
+        Paragraph::new(text)
+            .style(Style::default().fg(Color::Black))
+            .alignment(Alignment::Center)
+            .block(block)
+            .render(area, buf);
+    }
+}
